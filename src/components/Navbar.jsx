a1124c453d55ba7ea1c1cd1d6738d64de7d38e27
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-11-27 12:24:32 -0700

    added more content to navbar and edited tab title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,22 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        
+        <Link 
+          to="/" 
+          classname="flex items-center gap-2" 
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0,0);
+          }}>
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18] font-bold cursor-pointer">Ivan Ramirez | Fullstack Developer</p>
+          </Link>
+          <p className="text-red-500">hello!</p>
       </div>
     </nav>
   )
