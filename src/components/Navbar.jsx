c9bace54503bb2aa6752f1d5a9363bb8bf327d25
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-11-27 12:45:30 -0700

    added nav links and hamburger menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -20,7 +21,29 @@ const Navbar = () => {
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[18] font-bold cursor-pointer">Ivan Ramirez | Fullstack Developer</p>
           </Link>
-          <p className="text-red-500">hello!</p>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((Link) => (
+              <li 
+                key={Link.id} 
+                className={`${
+                  active === Link.title
+                    ? "text-white"
+                    : "text-secondary"
+                    } hover:text-white text-[18px] font-medium cursor-pointer`}
+                    onClick={() => setActive(Link.title)}
+                    >
+                <a href={`#${Link.id}`}>{Link.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img 
+              src={menu}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain cursor-pointer"
+              onClick={() => setToggle(!toggle)}
+            />
+          </div>
       </div>
     </nav>
   )
